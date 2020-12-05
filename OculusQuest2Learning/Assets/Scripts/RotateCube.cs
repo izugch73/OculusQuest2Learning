5d9c411254c2f39d044c7e1a70a369095cb946d1
Izumi Shirosaki <izugch73@gmail.com>
2020-12-05 19:28:37 +0900

    まわる

---
+++ OculusQuest2Learning/Assets/Scripts/RotateCube.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Leaning
+{
+    public class RotateCube : MonoBehaviour
+    {
+
+        void Start()
+        {
+
+            transform.DORotate(new Vector3(0f, 0f, -360f), 1.0f, RotateMode.FastBeyond360).SetLoops(-1)
+                .SetEase(Ease.Linear);
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.RotateAround(Vector3.zero, Vector3.up, 0.5f);
+        
+        }
+    }
+    
+}
